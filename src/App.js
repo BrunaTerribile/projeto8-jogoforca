754b7bbc1d49c70aecd2d6e90b720eabbd7b5fc1
Bruna Terribile <brunaterribile@gmail.com>
2022-09-23 22:17:32 -0300

    Layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import './palavras'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <main>
+        <img className="hangman" src='./img/forca0.png'></img>
+        <div className="side">
+          <div> <button>Escolher Palavra</button> </div>
+          <div className="word"> b_t_t_ </div>
+        </div>
+
+      </main>
+
+      <div className="bottomBar">
+        <Keyboard></Keyboard>
+
+        <div className="shot">
+          <p>Já sei a palavra!</p>
+          <input></input>
+          <button>Chutar</button>
+        </div>
+      </div>
+    </>
+  )
 }
+
+function Keyboard() {
+
+  const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+  return (
+    <div className="keyboard">
+      {alfabeto.map(letra => <div className='letter'>{letra.toUpperCase()}</div>)}
+    </div>
+
+  )
+
+
+}
+
+
+
+
 
 export default App;
